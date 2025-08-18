3fafddfbb36e6d8cb7fe20ffe5b0e9b144015b86
ankitmukhia <ankitmukhia157@gmail.com>
2025-08-19 01:50:17 +0530

    fix ai layout

--- components/ankit-ai.tsx
+++ components/ankit-ai.tsx
@@ -35,8 +35,8 @@ export const AnkitAI = () => {
     <div className="relative flex flex-col h-[600px] text-sm font-satoshi">
       <div className="absolute right-0 overflow-y-auto bg-blue-500 gradient-circle" />
       {messages.length === 0 && (
-        <div className="absolute left-4 max-w-xs top-6 border-body/5 flex-1 bg-green-300/30 overflow-y-auto rounded-xl border p-4">
-          <p className="text-sm text-green-300 font-manrope">
+        <div className="absolute left-4 max-w-xs top-6 border border-body/5 flex-1 overflow-y-auto tracking-wider rounded-tr-2xl rounded-bl-2xl rounded-br-2xl p-4">
+          <p className="text-sm font-manrope">
             Hi, I am ankit&apos;s AI, ask me anything about him!
           </p>
         </div>
@@ -54,10 +54,10 @@ export const AnkitAI = () => {
 			    ${msg.role === "user" ? "justify-end" : "justify-start"}`}
               >
                 <div
-                  className={`${
+                  className={`max-w-xs${
                     msg.role === "user"
-                      ? "rounded-full border border-body/5 bg-orange-300/30 text-orange-300"
-                      : "rounded-full border border-body/5 bg-green-300/30 text-green-300"
+                      ? "tracking-wider border border-white/5 rounded-tl-2xl rounded-br-2xl rounded-bl-2xl"
+                      : "tracking-wider border border-body/5 rounded-tr-2xl rounded-bl-2xl rounded-br-2xl"
                   } max-w-xs rounded-lg px-2.5 py-1.5`}
                 >
                   <p>{msg.content}</p>
@@ -69,22 +69,20 @@ export const AnkitAI = () => {
       </div>
 
       <GridTopLayout />
+      {/* focus-within:outline-1 focus-within:outline-gray-500/50 */}
 
-      <form
-        className="relative focus-within:outline-1 focus-within:outline-gray-500/50 w-full"
-        onSubmit={handleSubmit}
-      >
+      <form className="relative w-full" onSubmit={handleSubmit}>
         <input
           ref={inputRef}
           className="w-full h-14 pl-4 outline-none placeholder:font-geist"
           value={input}
-          placeholder="ask me anything..."
+          placeholder="ask me anything... and press enter ↵"
           onChange={handleInputChange}
         />
 
         <button
           type="submit"
-          className="absolute right-1 top-1/2 -translate-y-1/2 h-10 w-10 rounded-full bg-hoverColor/40 flex items-center justify-center"
+          className="absolute right-1 top-1/2 -translate-y-1/2 h-10 w-10 cursor-pointer rounded-full bg-hoverColor/40 flex items-center justify-center"
         >
           <ArrowUpIcon className="w-4 h-4 text-blue-300" />
         </button>
