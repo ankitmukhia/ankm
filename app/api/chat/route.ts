f1962b964e0d5374872f0d9b851965a26f459cba
ankitmukhia <ankitmukhia157@gmail.com>
2025-12-15 12:53:04 +0530

    ai model change, fixed response not streaming

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -8,7 +8,7 @@ export async function POST(req: Request) {
   const { messages } = await req.json();
 
   const result = streamText({
-    model: google("gemini-2.0-flash-001"),
+    model: google("gemini-2.5-flash"),
     system: SystemPrompt(),
     messages,
   });
